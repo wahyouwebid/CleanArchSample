9c3bbe76ee7f112b2423cba00c881d44ab0c4cb4
Ujang Wahyu <uw24318x@cimbniaga.co.id>
2023-09-21 07:10:26 +0700

    fix: remove un used function

--- app/src/main/java/com/ujangwahyu/posttest/core/sharepref/SharedPref.kt
+++ app/src/main/java/com/ujangwahyu/posttest/core/sharepref/SharedPref.kt
@@ -20,7 +20,6 @@ class SharedPref constructor(
     val context: Context
 ) {
     companion object {
-        const val prefName = "id.co.cimbniaga.mobile.android_preferences"
         const val keyAlias: String = "_androidx_security_master_key_"
         const val secretSharedPref: String = "secret_shared_prefs"
 
@@ -57,46 +56,12 @@ class SharedPref constructor(
         editor = pref.edit()
     }
 
-    fun batch() = pref
-
     fun putString(key: String, value: String?) {
         editor!!.putString(key, value)
         editor!!.commit()
     }
 
-    fun putInt(key: String, value: Int) {
-        editor!!.putInt(key, value)
-        editor!!.commit()
-    }
-
-    fun putLong(key: String, value: Long) {
-        editor!!.putLong(key, value)
-        editor!!.commit()
-    }
-
-    fun putBoolean(key: String, value: Boolean) {
-        editor!!.putBoolean(key, value)
-        editor!!.commit()
-    }
-
     fun getString(key: String): String {
         return pref.getString(key, "").toString()
-    }
-
-    fun getInt(key: String): Int {
-        return pref.getInt(key, 0)
-    }
-
-    fun getInt(key: String, defValue: Int = 0): Int {
-        return pref.getInt(key, defValue)
-    }
-
-    fun getBoolean(key: String): Boolean {
-        return pref.getBoolean(key, false)
-    }
-
-    fun logOut() {
-        editor?.clear()
-        editor?.commit()
     }
 }
